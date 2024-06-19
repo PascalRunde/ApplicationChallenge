870e597f7a55a7c648c5cea3587747c5fb9c7974
Pascal Runde <pascal_runde@web.de>
2024-06-19 11:38:58 +0200

    Setup for database test

--- DropletForms/DropletForms/Program.cs
+++ DropletForms/DropletForms/Program.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Data.SQLite;
+
 namespace DropletForms
 {
     internal static class Program
@@ -8,10 +11,9 @@ namespace DropletForms
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new DropletMain());
+            Application.Run(new AddImageForm());
         }
+
     }
 }
