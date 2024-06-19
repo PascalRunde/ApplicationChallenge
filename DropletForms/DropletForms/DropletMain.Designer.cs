9f95aa3ebce7bc97829c6d46594baf5f8a872569
Pascal Runde <pascal_runde@web.de>
2024-06-19 11:37:49 +0200

    Change main form

--- DropletForms/DropletForms/DropletMain.Designer.cs
+++ DropletForms/DropletForms/DropletMain.Designer.cs
@@ -29,22 +29,93 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DropletMain));
+            button1 = new Button();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
             SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.White;
+            button1.FlatAppearance.MouseDownBackColor = Color.FromArgb(224, 224, 224);
+            button1.FlatAppearance.MouseOverBackColor = Color.Silver;
+            button1.ForeColor = SystemColors.ControlText;
+            button1.Location = new Point(43, 92);
+            button1.Name = "button1";
+            button1.Size = new Size(161, 80);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(275, 92);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(696, 464);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(1010, 92);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(696, 464);
+            pictureBox2.TabIndex = 2;
+            pictureBox2.TabStop = false;
+            pictureBox2.Click += pictureBox2_Click;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Location = new Point(275, 592);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(696, 464);
+            pictureBox3.TabIndex = 4;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Location = new Point(1010, 592);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(696, 464);
+            pictureBox4.TabIndex = 3;
+            pictureBox4.TabStop = false;
             // 
             // DropletMain
             // 
             AutoScaleDimensions = new SizeF(17F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
-            ClientSize = new Size(1234, 736);
+            ClientSize = new Size(1748, 1101);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(button1);
             Font = new Font("Verdana", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
             Icon = (Icon)resources.GetObject("$this.Icon");
             Margin = new Padding(6, 5, 6, 5);
             Name = "DropletMain";
             Text = "Droplet";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Button button1;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
     }
 }
--- DropletForms/DropletForms/DropletMain.cs
+++ DropletForms/DropletForms/DropletMain.cs
@@ -5,6 +5,18 @@ namespace DropletForms
         public DropletMain()
         {
             InitializeComponent();
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
