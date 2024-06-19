112f38a6e413ded91f73a272c74400fcf7e6d09d
Pascal Runde <pascal_runde@web.de>
2024-06-19 11:32:27 +0200

    Add new fields to Imagemodel
    Add ID as GUID and Filepath
    Add a constructor as well

--- DropletForms/DropletLib/ImageModel.cs
+++ DropletForms/DropletLib/ImageModel.cs
@@ -1,17 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DropletLib
 {
     public class ImageModel
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
         public string Name { get; set; }
         public string Location { get; set; }
+        public string Filepath { get; set; }
         public int Rating { get; set; }
 
+        public ImageModel() 
+        {
+            Id = Guid.NewGuid();
+            Rating = 0;
+        }
+
+        public ImageModel(string filepath) : this() 
+        {
+            Filepath = filepath;
+        }
+
+        public ImageModel(string name, string location, string filepath) : this(filepath) 
+        {
+            Name = name;
+            Location = location;
+        }
     }
 }
