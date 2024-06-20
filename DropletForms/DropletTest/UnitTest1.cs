630e9e3c73c1e7b3289012f37dc296e21d5dd642
Pascal Runde <pascal_runde@web.de>
2024-06-20 09:45:01 +0200

    Add some tests

--- DropletForms/DropletTest/UnitTest1.cs
+++ DropletForms/DropletTest/UnitTest1.cs
@@ -1,6 +1,9 @@
 using DropletForms.DBCommunication;
 using DropletForms.RatingCalculation;
 using DropletLib;
+using System.Data.SQLite;
+using System.Data;
+using Dapper;
 
 namespace DropletTest
 {
@@ -8,18 +11,47 @@ namespace DropletTest
     {
         IDatabaseCommunicationService databaseCommunicationService;
         RatingCalculator ratingCalculator;
+        System.Data.IDbConnection connection;
         public UnitTest1()
-        {
-            databaseCommunicationService = new DatabaseCommunicationService(@"Data Source=.\Data\DropletSQLite.db");
+        { 
+            string connectionString = @"Data Source=.\Data\DropletSQLite.db";
+            databaseCommunicationService = new DatabaseCommunicationService(connectionString);
             ratingCalculator = new RatingCalculator(databaseCommunicationService);
+            this.connection = new SQLiteConnection(connectionString);
         }
 
         [Fact]
-        public void AddImagenAndReadImage()
+        public void AddImage_Test()
         {
-            databaseCommunicationService.AddImage("test");
-            var image = databaseCommunicationService.GetHighestRatedImages().FirstOrDefault();
+            ImageModel expectedModel = new ImageModel("Image", "Germany", "C:/testfile");
+            
+            databaseCommunicationService.AddImage("Image", "Germany", "C:/testfile");
+            
+            ImageModel model = connection.Query<ImageModel>("SELECT * FROM Images", new DynamicParameters()).First();
+
+            Assert.Equal(expectedModel.Name, model.Name);
+            Assert.Equal(expectedModel.Filepath, model.Filepath);
+            Assert.Equal(expectedModel.Location, model.Location);
+            Assert.Equal(expectedModel.Rating, model.Rating);
         }
+
+        [Fact]
+        public void ImproveImageRating_Test()
+        {
+
+            databaseCommunicationService.AddImage("Image", "Germany", "C:/testfile");
+
+            ImageModel model = connection.Query<ImageModel>("SELECT * FROM Images", new DynamicParameters()).First();
+
+            int expectedRating = model.Rating + 1;
+
+            databaseCommunicationService.ImproveImageRating(model);
+
+            ImageModel improvedModel = connection.Query<ImageModel>("SELECT Rating FROM Images WHERE Id = @Id", model).First();
+
+            Assert.Equal(expectedRating, improvedModel.Rating);
+        }
+
         public void Dispose()
         {
             databaseCommunicationService.Dispose();
